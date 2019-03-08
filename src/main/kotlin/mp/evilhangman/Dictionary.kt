60c949035de98601cbfaba1c5f5b146b4e422303
Meet Parikh <m.parikh@reply.com>
2019-03-08 17:26:40 +0000

    Blueprint guess function

--- src/main/kotlin/mp/evilhangman/Dictionary.kt
+++ src/main/kotlin/mp/evilhangman/Dictionary.kt
@@ -9,10 +9,30 @@ class Dictionary {
     private fun getWordMap(): Map<Int, List<String>> = words.groupBy({ it.length }, { it })
 
     var guessCount = 0
-    var guessedChars = mutableListOf<Char>()
+    var guessedChars = mutableSetOf<Char>()
     val actualWordLength = getWordMap().keys.random()
     var eligibleWords = getWordMap()[actualWordLength]
     val actualWord = eligibleWords!!.random()
+
+    fun guess(c: Char) {
+        if (c in guessedChars) {
+            println("Character already guessed earlier.")
+        } else {
+            //Increment guessCount
+            guessCount++
+
+            //Add to guessed chars
+            guessedChars.add(c)
+
+            //generate word families
+
+            //select a word family
+
+            //modify eligible words accordingly
+
+            //winner if actualWord is the guessedWord
+        }
+    }
 
 }
 
