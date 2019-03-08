f873a0f80566039abfc6173bf16e141a083c718c
Meet Parikh <m.parikh@reply.com>
2019-03-08 17:11:33 +0000

    Implementing game related variables

--- src/main/kotlin/mp/evilhangman/App.kt
+++ src/main/kotlin/mp/evilhangman/App.kt
@@ -2,9 +2,7 @@ package mp.evilhangman
 
 fun main() {
     val dictionary = Dictionary()
-    println(dictionary.words.size)
-    println(dictionary.getWordMap()[2])
-    println(dictionary.getRandomLength())
-    println(dictionary.getRandomLength())
-    println(dictionary.getRandomLength())
+    println(dictionary.actualWordLength)
+    println(dictionary.actualWord)
+    println(dictionary.eligibleWords)
 }
--- src/main/kotlin/mp/evilhangman/Dictionary.kt
+++ src/main/kotlin/mp/evilhangman/Dictionary.kt
@@ -4,10 +4,15 @@ import java.io.File
 
 class Dictionary {
 
-    val words: List<String> = File(javaClass.classLoader.getResource("dictionary.txt").file).readLines()
+    private val words: List<String> = File(javaClass.classLoader.getResource("dictionary.txt").file).readLines()
 
-    fun getWordMap(): Map<Int, List<String>> = words.groupBy({ it.length }, { it })
+    private fun getWordMap(): Map<Int, List<String>> = words.groupBy({ it.length }, { it })
 
-    fun getRandomLength(): Int = getWordMap().keys.random()
+    var guessCount = 0
+    var guessedChars = mutableListOf<Char>()
+    val actualWordLength = getWordMap().keys.random()
+    var eligibleWords = getWordMap()[actualWordLength]
+    val actualWord = eligibleWords!!.random()
+
 }
 
